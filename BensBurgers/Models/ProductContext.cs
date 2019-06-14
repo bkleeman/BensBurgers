0112a9a32564c456622f9d82eeedd595fb462e46
bkleeman <benjamin.kleeman@gmail.com>
2019-06-13 20:55:50 -0500

    Added ShoppingCartItems to the ProductContext class to CRUD on the CartItem model

--- BensBurgers/Models/ProductContext.cs
+++ BensBurgers/Models/ProductContext.cs
@@ -5,9 +5,10 @@ namespace BensBurgers.Models
     {
         public ProductContext() : base("BensBurgers")
         {
-            var x = 1;
+            //var x = 1; used for debugging, comment out and set a breakpoint to see if the page is generated
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
     }
 }
