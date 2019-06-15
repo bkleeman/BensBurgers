75e88636fcdc0e79a206ac6ae1c44dd024618c13
bkleeman <benjamin.kleeman@gmail.com>
2019-06-14 22:56:43 -0500

    Prepping for OAuth -- just enabled Google Authentication on startup, still gotta get the API key though

--- BensBurgers/App_Start/Startup.Auth.cs
+++ BensBurgers/App_Start/Startup.Auth.cs
@@ -59,11 +59,11 @@ namespace BensBurgers
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "",
+                ClientSecret = ""
+            });
         }
     }
 }
